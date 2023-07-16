883e27fa9a33cfa5568d8cc96fdc93416b058bb5
ianotee <oteeian500@gmail.com>
2023-07-16 11:47:20 +0300

    auth

---
+++ main/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Image(models.Model):
+    title = models.CharField(max_length =100)
+    image = models.ImageField(upload_to='image/')
+
+
+    def __str__ (self):
+     return self.title
+
